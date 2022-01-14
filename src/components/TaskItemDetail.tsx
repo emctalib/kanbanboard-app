52ce81bed1b83cf942884abd59a56dc6e115e56b
emctalib <emc_talib@hotmail.com>
2022-01-14 22:05:58 +0500

    f

--- src/components/TaskItemDetail.tsx
+++ src/components/TaskItemDetail.tsx
@@ -20,16 +20,30 @@ const TaskItemDetail: FC<TaskItemDetailProps> = ({ item, showDialog, onShowDialo
         onShowDialog(true);
     };
 
+    const updateInputValue = (event: React.MouseEvent<HTMLInputElement>) => {
+        alert(event);
+        //const { target: { value } } = event;
+        //this.setState({ recipeName: value });
+    }
+    //https://jasonwatmore.com/post/2020/10/14/react-hook-form-combined-add-edit-create-update-form-example
     return (
         <>
-            <Modal show={show} onHide={handleClose} centered>
+            <Modal show={show} onHide={handleClose} centered size='lg'>
                 <Modal.Header closeButton>
                     <Modal.Title>
-                        <p contentEditable>{item?.title} - Task Details</p>
+                        <p contentEditable>Task Details</p>
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <p contentEditable>{item?.description}</p> </Modal.Body>
+                    <div className="form-group">
+                        <label htmlFor="tbTitle">Title:</label>
+                        <input type="text" className="form-control" id="tbTitle" value={item?.title} onChange={() => updateInputValue} placeholder="Please enter title." />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="tbDesc">Description:</label>
+                        <input type="text" className="form-control" value={item?.description} id="tbDesc" placeholder="Please enter description." />
+                    </div>
+                    <p contentEditable></p> </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
